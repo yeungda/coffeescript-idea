0f4950931a4303316587ae793b45c417b728f464
Michael Kessler <michi@netzpiraten.ch>
2010-10-19 14:28:59 +0200

    Commenting and uncommentig of line comments

---
+++ src/coffeescript/lang/Commenter.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package coffeescript.lang;
+
+import com.intellij.lang.CodeDocumentationAwareCommenter;
+import com.intellij.psi.PsiComment;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.Nullable;
+
+public class Commenter implements CodeDocumentationAwareCommenter {
+
+  public String getLineCommentPrefix() {
+    return "#";
+  }
+
+  public String getBlockCommentPrefix() {
+    return null;
+  }
+
+  public String getBlockCommentSuffix() {
+    return null;
+  }
+
+  public String getCommentedBlockCommentPrefix() {
+    return null;
+  }
+
+  public String getCommentedBlockCommentSuffix() {
+    return null;
+  }
+
+  @Nullable
+  public IElementType getLineCommentTokenType() {
+    return coffeescript.lang.Tokens.COMMENT;
+  }
+
+  @Nullable
+  public IElementType getBlockCommentTokenType() {
+    return null;
+  }
+
+  @Nullable
+  public IElementType getDocumentationCommentTokenType() {
+    return null;
+  }
+
+  @Nullable
+  public String getDocumentationCommentPrefix() {
+    return null;
+  }
+
+  @Nullable
+  public String getDocumentationCommentLinePrefix() {
+    return null;
+  }
+
+  @Nullable
+  public String getDocumentationCommentSuffix() {
+    return null;
+  }
+
+  public boolean isDocumentationComment(PsiComment element) {
+    return false;
+  }
+}
